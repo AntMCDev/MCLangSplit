df769e155c189c90ad1c77979108e75c17288433
AntCasey <antcasey.developer@gmail.com>
2021-06-06 17:58:29 +0100

    Exteneded dynamic GUI string resize to affect generic widgets, as opposed to just buttons. Added a new config option to exclude certain language keys from the dual translation set.

--- src/main/java/com/ant/mclangsplit/config/ConfigHandler.java
+++ src/main/java/com/ant/mclangsplit/config/ConfigHandler.java
@@ -3,6 +3,9 @@ package com.ant.mclangsplit.config;
 import net.minecraftforge.common.ForgeConfigSpec;
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConfigHandler {
     public static final Common COMMON;
     public static final ForgeConfigSpec COMMON_SPEC;
@@ -14,10 +17,12 @@ public class ConfigHandler {
 
     public static class Common {
         public final ForgeConfigSpec.ConfigValue<String> languageSetting;
+        public final ForgeConfigSpec.ConfigValue<List<String>> ignoreKeys;
 
         public Common(ForgeConfigSpec.Builder builder) {
             builder.push("Language Settings");
             languageSetting = builder.comment("Set this to the second language").define("secondlanguage", "");
+            ignoreKeys = builder.comment("Use this to ignore a set of language keys for dual translation").define("ignoreKeys", new ArrayList<>());
             builder.pop();
         }
     }
